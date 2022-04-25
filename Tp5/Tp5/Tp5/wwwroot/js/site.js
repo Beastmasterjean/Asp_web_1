e21c69ac1ec760cf844ac1dae53d87c3d4d20e2f
Beastmasterjean <91993638+Beastmasterjean@users.noreply.github.com>
2022-04-25 14:45:23 -0400

    modification pas fini datepicker
    modification pas fini datepicker

--- Tp5/Tp5/Tp5/wwwroot/js/site.js
+++ Tp5/Tp5/Tp5/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+<script>
+    $( function() {
+        $("#datepicker").datepicker({ format: 'mm/dd/yyyy'})
+  } );
+  </script>
